86a63e7b4d046a32344fd6800298d6c1494d0bef
wibbuffey <wobbuffet000@gmail.com>
2021-10-24 22:36:03 -0400

    v1.1.0

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const core = require("@actions/core");
 const jsdoc2md = require("jsdoc-to-markdown");
+const fs = require("fs");
 
 try {
   const source = core.getInput("source");
+  const output = core.getInput("output");
+  const result = jsdoc2md.render({ files: source });
 
-  core.setOutput("result", jsdoc2md.render({ files: source }));
+  core.setOutput("result", result);
+  fs.writeFileSync(output, result);
 } catch (error) {
   core.setFailed(error.message);
 }
